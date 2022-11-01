7010e1f3d9866b78bd8905b23bcfbde652b3d11f
Vinicius Fortunato <vinicius.fortunato@grupoboticario.com>
2022-11-01 18:36:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# JS Concepts
+
+This repo contains examples of basic JS concepts.
+
+Content:
+
+1. Variables
+2. Constants
+3. Basic types
+4. Conditionals
+5. Arithmetic Operators
+6. Logical Operators
+7. Loops
+
---
+++ types/script.js
@@ -0,0 +1,11 @@
+// Number
+const age = 18;
+console.log('age = ', age);
+
+// String
+const username = 'Vinicius';
+console.log('username =', username);
+
+// Boolean
+const isHuman = true;
+console.log('isHuman = ', isHuman);
